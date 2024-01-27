2490580975480e2ea528ac47a95cf120f3b8ea01
Thom988 <t.quillet@hotmail.fr>
2024-01-27 21:32:40 +0100

    Mon premier commit pour mettre le projet en ligne

---
+++ src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
@@ -0,0 +1,17 @@
+package com.ecommerce.microcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+// @SpringBootApplication = @Configuration + @EnableAutoConfiguration + @ComponentScan
+
+@SpringBootApplication
+public class MicrocommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrocommerceApplication.class, args);
+	}
+
+}
